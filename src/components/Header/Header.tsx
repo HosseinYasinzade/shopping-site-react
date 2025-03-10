3e5489179f1e5978d8714230e939e1b6cf68d1b3
Hossein Yasinzadeh <hosseinyasinzadeh2002@gmail.com>
2025-03-10 19:06:04 +0330

    feat: codding nav

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,12 @@
 import HeaderTop from "../HeaderTop/HeaderTop";
+import Nav from "../Nav/Nav";
 import Hero from "../Hero/Hero";
 
 const Header = () => {
   return (
     <>
       <HeaderTop />
+      <Nav />
       <Hero />
     </>
   );
---
+++ src/components/Nav/Nav.tsx
@@ -0,0 +1,22 @@
+const Nav = () => {
+  return (
+    <nav className="flex justify-between items-center px-30 w-full h-20">
+      <h3 className="text-3xl font-bold">Logo</h3>
+      <input
+        type="text"
+        placeholder="Search for product"
+        className="border-b border-green-800 outline-0"
+      />
+      <div>
+        <button className="bg-green-800 text-white text-lg w-20 h-10 rounded-xl mr-2">
+          Login
+        </button>
+        <button className="bg-green-800 text-white text-lg w-20 h-10 rounded-xl">
+          Register
+        </button>
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
